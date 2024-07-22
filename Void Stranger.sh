5c38d516aca0c5d0337584cc5c94c357bfc16f6f
EzDzzIt <143894375+EzDzzIt@users.noreply.github.com>
2024-07-22 10:42:50 -0500

    main script update
    also consolidated lib folders

--- Void Stranger.sh
+++ Void Stranger.sh
@@ -15,7 +15,7 @@ fi
 source $controlfolder/control.txt
 source $controlfolder/device_info.txt
 export PORT_32BIT="Y"
-export LD_LIBRARY_PATH="/usr/lib:/usr/lib32:/$GAMEDIR/libs:$LD_LIBRARY_PATH"
+export LD_LIBRARY_PATH="/usr/lib:/usr/lib32:/$GAMEDIR/lib:$LD_LIBRARY_PATH"
 
 get_controls
 [ -f "${controlfolder}/mod_${CFW_NAME}.txt" ] && source "${controlfolder}/mod_${CFW_NAME}.txt"
@@ -33,70 +33,66 @@ export GMLOADER_PLATFORM="os_linux"
 # log the execution of the script into log.txt
 exec > >(tee "$GAMEDIR/log.txt") 2>&1
 
-game_chksm=$(md5sum gamedata/"data.win" | awk '{print $1}')
-patch_chksm=$(md5sum gamedata/"vs.xdelta" | awk '{print $1}')
-itch_chksm=$(md5sum gamedata/itch/"vs-itch-to-steam.xdelta" | awk '{print $1}')
-audio1_chksm=$(md5sum gamedata/"audiogroup1.dat" | awk '{print $1}')
-audio2_chksm=$(md5sum gamedata/"audiogroup2.dat" | awk '{print $1}')
-data_chksm=$(md5sum gamedata/"voidstranger_data.csv" | awk '{print $1}')
-
-# Patch the data.win file, with support for ItchIo version
-if [ -f "gamedata/vs-patched.win" ]; then
-  echo "Found patched data file."
-elif [[ -f "gamedata/data.win" ]] && [[ "$game_chksm" = "29f820538024539f18171fb447034fe7" ]]; then
-  echo "Patching Steam Version data.win"
-  $ESUDO $controlfolder/xdelta3 -d -s gamedata/"data.win" gamedata/"vs.xdelta" gamedata/"vs-patched.win"
-elif [[ -f "gamedata/data.win" ]] && [[ "$game_chksm" = "1a666b533539af4cebb7c12311bd9a56" ]]; then
-  echo "Itch Version Found. Patching to be equivalent to Steam version."
-  mv gamedata/"data.win" gamedata/itch/"data_itch.win"
-  $ESUDO $controlfolder/xdelta3 -d -s gamedata/itch/"data_itch.win" gamedata/itch/"vs-itch-to-steam.xdelta" gamedata/"data.win"
-  if [ -f "gamedata/data.win" ]; then
-    echo "Patching Updated data.win"
-    $ESUDO $controlfolder/xdelta3 -d -s gamedata/"data.win" gamedata/"vs.xdelta" gamedata/"vs-patched.win"
+# Check to see if a patched game already exists at the start, and verify with current patch version:
+if [ -f "game.droid" ]; then
+  final_chksm=$(md5sum "game.droid" | awk '{print $1}')
+  if [ "$final_chksm" = "55f1636359a01fa3a3d0fda583e7f351" ]; then
+    echo "Found patched game file. Checksum good. ""$final_chksm"
   else
-    echo "Error while patching. Ensure all required files are in place."
+    echo "game.droid checksum does not match; md5: ""$final_chksm"
+    exit 0
   fi
 else
-  echo "Incorrect game checksum or game data not found; check the instructions and your game version."
+  # get data.win checksum
+  game_chksm=$(md5sum gamedata/"data.win" | awk '{print $1}')
+  # verify if Steam Version, then patch
+  if [[ -f "gamedata/data.win" ]] && [[ "$game_chksm" = "29f820538024539f18171fb447034fe7" ]]; then
+    echo "Steam Version Found. Patching data.win. data.win md5 ""$game_chksm"
+    $ESUDO $controlfolder/xdelta3 -d -s gamedata/"data.win" gamedata/"vs.xdelta" gamedata/"vs-patched.win"
+  # or check if it's the Itch version, patch for Steam Version parity and then patch again
+  elif [[ -f "gamedata/data.win" ]] && [[ "$game_chksm" = "1a666b533539af4cebb7c12311bd9a56" ]]; then
+    echo "Itch Version Found. Patching to be equivalent to Steam version. data.win md5 ""$game_chksm"
+    mv gamedata/"data.win" gamedata/"data_itch.win"
+    $ESUDO $controlfolder/xdelta3 -d -s gamedata/"data_itch.win" gamedata/itch/"vs-itch-to-steam.xdelta" gamedata/"data.win"
+    echo "Patching Updated data.win"
+    $ESUDO $controlfolder/xdelta3 -d -s gamedata/"data.win" gamedata/"vs.xdelta" gamedata/"vs-patched.win"
+    echo "Cleaning up."
+    $ESUDO rm gamedata/"data_itch.win"
+  else
+    echo "Incorrect game checksum or game data not found; check the instructions and your game version. data.win md5 ""$game_chksm"
+    exit 0
+  fi
 fi
 
-final_chksm=$(md5sum gamedata/"vs-patched.win" | awk '{print $1}')
-
-echo "Compare these checksum values to the included checksums_info.txt for troubleshooting."
-echo " "
-echo "Game md5: ""$game_chksm"
-echo "Main Patch md5: ""$patch_chksm"
-echo "Itch Patch md5: ""$itch_chksm"
-echo "Final Patched Game md5: ""$final_chksm"
-echo "audiogroup1 md5: ""$audio1_chksm"
-echo "audiogroup2 md5: ""$audio2_chksm"
-echo "data CSV md5: ""$data_chksm"
-echo " "
-
-
-# Check if there is an empty file called "loadedapk" in the dir
+# Check if there is an empty file called "loadedapk" in the dir, then add audio files if not
 if [ ! -f loadedapk ]; then
-
   echo "Zipping game files..."
-
-  $SUDO mkdir -p ./assets/
-
-  $SUDO mv gamedata/"audiogroup1.dat" ./assets/
-  $SUDO mv gamedata/"audiogroup2.dat" ./assets/
-  $SUDO mv gamedata/"splash.png" ./assets/
-  $SUDO mv gamedata/"voidstranger_data.csv" $GAMEDIR
-
+  $ESUDO mkdir -p ./assets/
+  $ESUDO mv gamedata/"audiogroup1.dat" ./assets/
+  $ESUDO mv gamedata/"audiogroup2.dat" ./assets/
+  # Splash Screen currently broken
+  # $ESUDO mv gamedata/"splash.png" ./assets/ 
   $ESUDO zip -r -0 $GAMEDIR/game.apk ./assets/
-
-  $SUDO rm -r ./assets/
+  $ESUDO rm -r ./assets/
   touch loadedapk
-
   echo "Files loaded in to .apk"
-
 fi
 
-# Check for file existence before trying to manipulate them:
-[ -f "./gamedata/vs-patched.win" ] && cp gamedata/vs-patched.win $GAMEDIR/game.droid
+# move csv data file to main dir and clean if the ini has been generated already
+if [ ! -f csvstring.ini ]; then
+  if [ ! -f voidstranger_data.csv ]; then
+    $ESUDO cp gamedata/"voidstranger_data.csv" ./
+    echo "CSV data file copied to main dir."
+  fi
+else
+  if [ -f voidstranger_data.csv ]; then
+    echo "Cleaning up CSV file."
+    $ESUDO rm "voidstranger_data.csv"
+  fi
+fi
+
+# Move patched file to main directory
+[ -f "./gamedata/vs-patched.win" ] && mv gamedata/vs-patched.win $GAMEDIR/game.droid
 
 # Make sure uinput is accessible so we can make use of the gptokeyb controls
 $ESUDO chmod 666 /dev/uinput
